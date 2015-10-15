c442a368b3d5ed459c6f7d1446259aeb077d581d
Braden <braden.douglass@gmail.com>
2015-10-15 15:25:59 -0400

    Augment Shipping Mailer
    The shipment handler that was introduced in spree 2.4 (I believe) has
    been removed in Solidus. Use Solidus' order shipping class to define a
    new send shipment emails method. Loop over cartons passed to this method
    to get at their order objects.

--- app/models/spree/shipment_handler_decorator.rb
+++ app/models/spree/shipment_handler_decorator.rb
@@ -1,17 +1,18 @@
-Spree::ShipmentHandler.class_eval do
+Spree::OrderShipping.class_eval do
 
     private
-    def send_shipped_email
-      #ShipmentMailer.shipped_email(id).deliver_later
-      order=@shipment.order
-      external_key = Spree::BrontoConfiguration.account[order.store.code]["order_shipped"]
+    def send_shipment_emails(carton)
+      carton.orders.each do |order|
+        #ShipmentMailer.shipped_email(id).deliver_later
+        order=@shipment.order
+        external_key = Spree::BrontoConfiguration.account[order.store.code]["order_shipped"]
 
-      DelayedSend.new( order.store.code,
-                       order.email,
-                       external_key,
-                       order.id.to_s,
-                       "order_mailer/order_shipped_plain",
-                       "order_mailer/order_shipped_html").perform
+        DelayedSend.new( order.store.code,
+                        order.email,
+                        external_key,
+                        order.id.to_s,
+                        "order_mailer/order_shipped_plain",
+                        "order_mailer/order_shipped_html").perform
+      end
     end
-
 end
