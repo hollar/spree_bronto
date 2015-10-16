3be423331fab1b2d7ec876a9f2ebd1a6bce7de13
Braden <braden.douglass@gmail.com>
2015-10-16 15:47:27 -0400

    Look Up Spree Store In Method
    The spree store will not be set in an ivar so lookup the current store
    in the method itself

--- app/mailers/spree/user_mailer_decorator.rb
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -1,7 +1,7 @@
 Spree::UserMailer.class_eval do
     def reset_password_instructions(user, token, *args)
       @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => token, :host => Spree::Store.current.url)
-      mail to: user.email, from: from_address, subject: "#{@store.name} #{I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions])}"
+      mail to: user.email, from: from_address, subject: "#{Spree::Store.current.name} #{I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions])}"
     end
 
 
