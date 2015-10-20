67515f0be9d1179b701dfe095afb847b53bf0f3b
Braden <braden.douglass@gmail.com>
2015-10-20 09:27:53 -0400

    Refactor The User Decorator
    User decorator code was all kinds of wonk. Likely in a test condition
    where the developers were spit-balling the overall interface. Break
    things out into nice, well worded methods to be more clear... yah know,
    with your words.

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,51 +1,77 @@
 module Spree
   User.class_eval do
-
-    has_and_belongs_to_many :bronto_lists, class_name: "Spree::BrontoList", join_table: :spree_bronto_lists_users
-
+    has_and_belongs_to_many :bronto_lists, class_name: 'Spree::BrontoList',
+                                           join_table: :spree_bronto_lists_users
     accepts_nested_attributes_for :bronto_lists
 
-    def deliver_password_reset_instructions!(current_store)
-
-      #@edit_password_reset_url = edit_spree_user_password_url(:reset_password_token => token, :host => current_store.url)
-
-      #mail to: user.email, from: from_address, subject: Spree::Store.current.name + ' ' + I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions])
-
-      #Bronto sending
-      #trigger the email directly here.
-      store_code= current_store.code
-      token= Spree::BrontoConfiguration.account[store_code]['token']
-      from_email= current_store.mail_from_address
-      from_name= Spree::BrontoConfiguration.account[store_code]['from_name']
-      reply_email= current_store.mail_from_address
-      email_options={:fromEmail =>from_email,:fromName => from_name, :replyEmail => reply_email}
-
-      user_order=orders.select{|o| o.state='complete'}.last
-
-      message_name= Spree::BrontoConfiguration.account[current_store.code]['password_reset']
-
-      attributes = {:First_Name => user_order.ship_address.firstname} unless !user_order
-      attributes = {:First_Name => user_order.ship_address.lastname} unless !user_order
-
-      attributes[:SENDTIME__CONTENT1] = reset_password_token
-      attributes[:SENDTIME__CONTENT2] = reset_password_token
-
-      begin
-        communication = BrontoIntegration::Communication.new(token)
-        communication.trigger_delivery_by_id(message_name,email,'transactional','html',attributes,email_options)
-
-      rescue => exception
-        begin    #handle the transactional contact in case the message is not approved for transactional.
-          contact = BrontoIntegration::Contact.new(token)
-          contact.update_status(email,'active')
-          communication.trigger_delivery_by_id(message_name,email,'triggered','html',attributes,email_options)
-        rescue => exception
-          raise exception
-        end
-      end
-
-
+    def send_devise_notification(notification, *args)
+      # @edit_password_reset_url = edit_spree_user_password_url(:reset_password_token => token, :host => current_store.url)
+      # mail to: user.email, from: from_address, subject: Spree::Store.current.name + ' ' + I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions])
+      # Bronto sending
+      # trigger the email directly here.
+      communication = BrontoIntegration::Communication.new(bronto_token)
+      communication.trigger_delivery_by_id(bronto_message, email,
+                                           'transactional', 'html',
+                                           message_attributes, email_options)
+    rescue
+      # handle the transactional contact in case the
+      # message is not approved for transactional.
+      contact = BrontoIntegration::Contact.new(bronto_token)
+      contact.update_status(email, 'active')
+      communication.trigger_delivery_by_id(bronto_message, email,
+                                           'triggered', 'html',
+                                           attributes, email_options)
+    rescue => exception
+      raise exception
     end
 
+    private
+
+    def current_store
+      _current_store ||= Spree::Store.current
+    end
+
+    def store_code
+      current_store.code
+    end
+
+    def email_options
+      { fromEmail: from_email, fromName: from_name, replyEmail: reply_email }
+    end
+
+    def from_email
+      current_store.mail_from_address
+    end
+
+    def recent_order
+      orders.complete.last
+    end
+
+    def from_name
+      bronto_config['from_name']
+    end
+
+    def reply_email
+      current_store.mail_from_address
+    end
+
+    def bronto_attributes
+      attributes = {}
+      attributes = {:First_Name => recent_order.ship_address.firstname} if recent_order
+      attributes = {:First_Name => recent_order.ship_address.lastname} if recent_order
+      attributes[:SENDTIME__CONTENT1] = reset_password_token
+    end
+
+    def bronto_message
+      bronto_config['password_reset']
+    end
+
+    def bronto_token
+      bronto_config['token']
+    end
+
+    def bronto_config
+      Spree::BrontoConfiguration.account[store_code]
+    end
   end
 end
