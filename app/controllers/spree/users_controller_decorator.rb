b1e99ea6740507bf92351609e94bb59779d0398d
Braden <braden.douglass@gmail.com>
2015-10-21 10:16:55 -0400

    Clean Up User Controller
    When the user modifies their bronto list membership, the controller
    should be able to react accordingly. Clean up the mess for
    check_newsletters and refactor many things out to methods and get rid of
    a lot of the conditionals.

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -4,24 +4,37 @@ Spree::UsersController.class_eval do
   before_action :load_bronto_lists
   before_action :check_newsletter
 
-private
+  private
+
+  attr_reader :bronto_lists, :list, :user
+
   def load_bronto_lists
-      @bronto_lists=current_store.bronto_lists
+    @bronto_lists = current_store.bronto_lists
   end
 
   def check_newsletter
-    if params['user'] && params['user']['bronto_list_attributes']
-      params['user']['bronto_list_attributes'].each do|k,v|
-        list=Spree::BrontoList.find(k)
-        if k && @user && v=="true"
-          subscribe_to_list(@user, list)
-        end
-
-        if k && @user && v=="false"
-          unsubscribe_from_list(@user, list)
-        end
-
-      end
+    return unless user_subscribing? && user
+    params['user']['bronto_list_attributes'].each do|k, v|
+      subscribe_user(list(k)) if v == 'true'
+      unsubscribe_user(list(k)) if v == 'false'
     end
+  end
+
+  def unsubscribe_user(list)
+    return unless list
+    unsubscribe_from_list(user, list)
+  end
+
+  def subscribe_user(list)
+    return unless list
+    subscribe_to_list(user, list)
+  end
+
+  def list(name)
+    Spree::BrontoList.find(name)
+  end
+
+  def user_subscribing?
+    params['user'] && params['user']['bronto_list_attributes']
   end
 end
