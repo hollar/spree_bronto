bdec7934640cf047bd276bf0f0ff33b531540b74
Braden <braden.douglass@gmail.com>
2015-10-21 10:48:15 -0400

    Clean Up Admin Bronto Controller
    Refactor the gigantic getter in the bronto controller. Move things out
    into their individual methods with memoization. Not sure if the list_id
    attr is needed but will keep it in there just in case.

--- app/controllers/spree/admin/bronto_lists_controller.rb
+++ app/controllers/spree/admin/bronto_lists_controller.rb
@@ -3,22 +3,37 @@ class Spree::Admin::BrontoListsController < Spree::Admin::ResourceController
 
   def get_lists
     @lists = []
-    @list_id = params[:list_id] if params.key? :list_id
-    @store_id = params[:store_id] if params.key? :store_id
-    @site= Spree::Store.find(@store_id)
-    bronto=Bronto.new(Spree::BrontoConfiguration.account[@site.code]['token'])
-    et_lists=bronto.read_lists
-    #et_list = ET::List.new(Spree::Config.get(:exact_target_user), Spree::Config.get(:exact_target_password))
-    et_lists.each do |etl|
-      #list = et_list.retrieve_by_id etl[:id]
+    bronto.read_lists.each do |etl|
       @lists << ["#{etl[:name]}", etl[:id]]
     end
-    render :partial => "get_lists", :layout => false
+    render partial: "get_lists", layout: false
   end
 
   private
 
+  attr_reader :bronto, :stores, :site, :store_id, :list_id
+
+  def bronto
+    @bronto ||= Bronto.new(bronto_config)
+  end
+
+  def site
+    @site ||= Spree::Store.find(store_id)
+  end
+
+  def store_id
+    @store_id ||= params[:store_id] if params.key? :store_id
+  end
+
+  def list_id
+    @list_id ||= params[:list_id] if params.key? :list_id
+  end
+
   def find_stores
-    @stores=Spree::Store.all
+    @stores ||= Spree::Store.all
+  end
+
+  def bronto_config
+    Spree::BrontoConfiguration.account[site.code]['token']
   end
 end
