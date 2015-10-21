8c03e2ef78201ba280a79def638ea9a1ff90ef97
Braden <braden.douglass@gmail.com>
2015-10-21 13:23:38 -0400

    Clean Up Delayed Sending Method.
    Delayed sending method needed a good amount of cleanup, and likely more
    to deal with delayed sending of order/shipping emails.

--- lib/delayed_send.rb
+++ lib/delayed_send.rb
@@ -1,40 +1,56 @@
 DelayedSend = Struct.new(:store_code, :email, :message_name, :order_id, :plain_view, :html_view) do
   def perform
     return if email.blank?
-    #store_code ||= 'default'
-    order = Spree::Order.find(order_id)
-    token= Spree::BrontoConfiguration.account[store_code]['token']
-    from_email= Spree::BrontoConfiguration.account[store_code]['from_address']
-    from_name= Spree::BrontoConfiguration.account[store_code]['from_name']
-    reply_email= Spree::BrontoConfiguration.account[store_code]['from_address']
-    email_options={:fromEmail =>from_email,:fromName => from_name, :replyEmail => reply_email}
+    bronto_api.trigger_delivery_by_id(message_name,
+                                      email,
+                                      'transactional',
+                                      'html',
+                                      bronto_attributes,
+                                      email_options)
+  rescue
+    # handle the transactional contact in case the message
+    # is not approved for transactional usage.
+    find_or_build_contact
+    bronto_api.trigger_delivery_by_id(message_name,
+                                      email,
+                                      'triggered',
+                                      'html',
+                                      bronto_attributes,
+                                      email_options)
+  rescue => exception
+    raise exception
+  end
 
+  def order
+    @order ||= Spree::Order.find(order_id)
+  end
 
-    view = ActionView::Base.new(Rails::Application::Configuration.new(Rails.root).paths["app/mailers/spree"])
-    view.view_paths<<File.join(File.dirname(__FILE__), '../app/mailers/spree')
+  def config
+    @config ||= Spree::BrontoConfiguration.account[store_code]
+  end
 
-    attributes = {:First_Name => order.bill_address.firstname,
-                 :Last_name => order.bill_address.lastname}
+  def bronto_api
+    @bronto_api ||= BrontoIntegration::Communication.new(config['token'])
+  end
 
-    attributes[:SENDTIME__CONTENT1] = view.render(plain_view, :order => order) unless plain_view.nil?
-    attributes[:SENDTIME__CONTENT2] = (view.render(html_view, :order => order)).gsub(/\n/,'').html_safe unless html_view.nil?
+  def find_or_build_contact(email)
+    contact = BrontoIntegration::Contact.new(config['token'])
+    contact.find_or_create(email)
+    contact.update_status(email, 'active')
+  end
 
-    begin
-      communication = BrontoIntegration::Communication.new(token)
-      communication.trigger_delivery_by_id(message_name,email,'transactional','html',attributes,email_options)
+  def bronto_attributes
+    attributes[:First_Name] = order.bill_address.firstname
+    attributes[:Last_Name] = order.bill_address.lastname
+  end
 
-    rescue => exception
-      begin    #handle the transactional contact in case the message is not approved for transactional.
-        contact = BrontoIntegration::Contact.new(token)
-        contact.update_status(email,'active')
-        communication.trigger_delivery_by_id(message_name,email,'triggered','html',attributes,email_options)
-      rescue => exception
-        raise exception
-      end
-    end
+  def email_options
+    @email_options ||= { fromEmail: config['from_address'],
+                         fromName: config['from_name'],
+                         replyEmail: config['from_address'] }
   end
 
   if Spree::BrontoConfiguration.account['handle_asynchronously']
-     handle_asynchronously :perform, :priority => 20
+    handle_asynchronously :perform, priority: 20
   end
 end
