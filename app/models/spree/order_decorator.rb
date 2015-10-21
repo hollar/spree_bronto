8cb9b65a7e44bc6682f30966744d9b061cb90018
Braden <braden.douglass@gmail.com>
2015-10-20 22:59:22 -0400

    Tweak Order Decorator
    Order decorator, so far makes the most sense out of all the rest of the
    decorators. Well right up until until running Rubocop which informed me
    that everything was using 4 spaces versus 2. Really, how low is your
    self esteem to use 4 spaces or tabs over 2? Feel free not to answer,
    because no answer could make any sense. Small amount of refactoring
    surrounding the external key and keeping things dry

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,30 +1,28 @@
 Spree::Order.class_eval do
+  def deliver_order_confirmation_email
+    DelayedSend.new(store.code,
+                    email,
+                    external_key['order_received'],
+                    id.to_s,
+                    'order_mailer/order_confirm_plain',
+                    'order_mailer/order_confirm_html').perform
 
-    def deliver_order_confirmation_email
-      #OrderMailer.confirm_email(id).deliver_later  # we replace OrderMailer with DelayedSend
+    update_column(:confirmation_delivered, true)
+  end
 
-      external_key = Spree::BrontoConfiguration.account[store.code]["order_received"]
-      DelayedSend.new( store.code,
-                       email,
-                       external_key,
-                       id.to_s,
-                       "order_mailer/order_confirm_plain",
-                       "order_mailer/order_confirm_html").perform
+  private
 
-      update_column(:confirmation_delivered, true)
-    end
+  def send_cancel_email
+    DelayedSend.new(store.code,
+                    email,
+                    external_key['order_canceled'],
+                    id.to_s,
+                    'order_mailer/order_cancel_plain',
+                    'order_mailer/order_cancel_html').perform
+  end
 
-    private
-    def send_cancel_email
-      #OrderMailer.cancel_email(id).deliver_later
-
-      external_key = Spree::BrontoConfiguration.account[store.code]['order_canceled']
-      DelayedSend.new( store.code,
-                       email,
-                       external_key,
-                       id.to_s,
-                       "order_mailer/order_cancel_plain",
-                       "order_mailer/order_cancel_html").perform
-    end
-
+  def external_key
+    _external_key ||= Spree::BrontoConfiguration
+                      .account[store.code]
+  end
 end
