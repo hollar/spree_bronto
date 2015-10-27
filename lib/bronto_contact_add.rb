8cdeac567c0d6db603bdeed035baac9f562fda9d
Braden <braden.douglass@gmail.com>
2015-10-26 20:55:46 -0400

    Create A Simple Contact Add For Bronto
    Simple contact add for Bronto. Used to acquire emails for people who my
    not want to currently be a user in the overall system. Used for many
    things like newsletter signup

---
+++ lib/bronto_contact_add.rb
@@ -0,0 +1,30 @@
+class BrontoContactAdd < ActiveJob::Base
+  def perform(email, store_code)
+    return if email.blank?
+    @email, @store_code = email, store_code
+    @contact = BrontoIntegration::Contact.new(bronto_token)
+
+    create_new_contact
+    assign_newsletter_status
+  end
+
+  private
+
+  attr_reader :contact, :email, :store_code
+
+  def bronto_token
+    bronto_config['token']
+  end
+
+  def bronto_config
+    Spree::BrontoConfiguration.account[store_code]
+  end
+
+  def create_new_contact
+    contact.find_or_create(email)
+  end
+
+  def assign_newsletter_status
+    contact.update_status(email, 'onboarding')
+  end
+end
