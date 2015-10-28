c7849655eeb1deb2b809c2d7b036545f65649dc5
Braden <braden.douglass@gmail.com>
2015-10-28 16:18:21 -0400

    Don't Send Async Emails Unless Set In Bronto
    Bronto config should toggle email delivery. If off, ActiveJob will
    remain dormant and send the normal mailers.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,21 +1,39 @@
 Spree::Order.class_eval do
   def deliver_order_confirmation_email
-    DelayedSend.perform_later(email,
-                              id,
-                              external_key['order_received'])
+    if handle_asynchronously?
+      send_email('order_received')
+    else
+      OrderMailer.confirm_email(self).deliver_later
+    end
     update_column(:confirmation_delivered, true)
   end
 
   private
 
+  def handle_asynchronously?
+    Spree::BrontoConfiguration['handle_asynchronously']
+  end
+
   def send_cancel_email
+    if handle_asynchronously?
+      send_email('order_canceled')
+    else
+      OrderMailer.cancel_email(self).deliver_later
+    end
+  end
+
+  def send_email(key)
     DelayedSend.perform_later(email,
                               id,
-                              external_key['order_canceled'])
+                              external_key[key])
   end
 
   def external_key
     _external_key ||= Spree::BrontoConfiguration
                       .account[store.code]
+  end
+
+  def store
+    Store.current
   end
 end
