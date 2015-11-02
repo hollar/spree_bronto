6bd590520fc207b7abb55073c708b3eaa2e155ba
Braden <braden.douglass@gmail.com>
2015-11-02 15:39:10 -0500

    Give Users Access To Street/Billing Info
    Pass the billing and shipping information to Bronto, allowing users to
    have access to this in mailers.

--- app/models/spree/order_mailer_attributes.rb
+++ app/models/spree/order_mailer_attributes.rb
@@ -8,6 +8,8 @@ module Spree
     def build_attributes
       build_message_tags
       build_line_item_tags
+      build_address_tags('bill')
+      build_address_tags('ship')
       build_payment_tags
       attrs
     end
@@ -26,10 +28,6 @@ module Spree
       attrs[:orderShippingTotals] = order.shipment_total.to_i
     end
 
-    def build_payment_tags
-      attrs[:paymentType] = order.payments.valid.first.source.cc_type
-    end
-
     def build_line_item_tags
       order.line_items.each_with_index do |item, idx|
         attrs[:"productImgUrl_#{idx}"] = item.variant.images.first.attachment.url(:small)
@@ -38,6 +36,34 @@ module Spree
         attrs[:"productQty_#{idx}"] = item.quantity
         attrs[:"productPriceExclTax_#{idx}"] = item.price.to_i
       end
+    end
+
+    def build_payment_tags
+      attrs[:paymentType] = order.payments.valid.first.source.cc_type
+      attrs[:paymentNumber] = order.payments.valid.first.source.last_digits
+    end
+
+    def build_address_tags(type)
+      attrs[:"#{type}Name"] = "#{address_for(type).firstname} #{address_for(type).lastname}"
+      address_street_block(type)
+      address_city_block(type)
+      attrs[:"#{type}Country"] = address_for(type).country.name
+    end
+
+    def address_street_block(type)
+      attrs[:"#{type}StreetAddress"] = address_for(type).address1
+      return unless address_for(type).address2
+      attrs[:"#{type}StreetAddress"] += "\n #{address_for(type).address2}"
+    end
+
+    def address_city_block(type)
+      attrs[:"#{type}CityLine"] = "#{address_for(type).city}, #{address_for(type).zipcode}"
+      return unless address_for(type).state
+      attrs[:"#{type}CityLine"] = "#{address_for(type).city}, #{address_for(type).state.name}, #{address_for(type).zipcode}"
+    end
+
+    def address_for(type)
+      order.send(:"#{type}_address")
     end
   end
 end
