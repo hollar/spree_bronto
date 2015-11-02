cc0ef368076c0cdce1309157d9ba5b1bb900e5b3
Braden <braden.douglass@gmail.com>
2015-11-02 14:12:51 -0500

    Wrap The Mailer In Spree Namespace
    Namespace required or errors will occur with lookup for the attributes.

--- app/models/spree/order_mailer_attributes.rb
+++ app/models/spree/order_mailer_attributes.rb
@@ -1,32 +1,43 @@
-class OrderMailerAttributes
-  def initialize(order)
-    @order = order
-    @attrs = {}
+module Spree
+  class OrderMailerAttributes
+    def initialize(order)
+      @order = order
+      @attrs = {}
+    end
+
+    def build_attributes
+      build_message_tags
+      build_line_item_tags
+      build_payment_tags
+      attrs
+    end
+
+    private
+
+    attr_reader :order, :attrs
+
+    def build_message_tags
+      attrs[:orderIncrementId] = order.number
+      attrs[:orderShippingDescription] = order.shipments.first.shipping_method.name
+      attrs[:shipmentTracking] = order.shipments.first.tracking
+      attrs[:orderTotals] = order.total.to_i
+      attrs[:orderSubTotals] = order.item_total.to_i
+      attrs[:orderTaxTotals] = order.tax_total.to_i
+      attrs[:orderShippingTotals] = order.shipment_total.to_i
+    end
+
+    def build_payment_tags
+      attrs[:paymentType] = order.payments.valid.first.source.cc_type
+    end
+
+    def build_line_item_tags
+      order.line_items.each_with_index do |item, idx|
+        attrs[:"productImgUrl_#{idx}"] = item.variant.images.first.attachment.url(:small)
+        attrs[:"productName_#{idx}"] = item.variant.name
+        attrs[:"productSku_#{idx}"] = item.variant.sku
+        attrs[:"productQty_#{idx}"] = item.quantity
+        attrs[:"productPriceExclTax_#{idx}"] = item.price.to_i
+      end
+    end
   end
-
-  def build_attributes
-    build_message_tags
-    build_loop_tags
-    attrs
-  end
-
-  private
-
-  attr_reader :order, :attrs
-
-  def build_message_tags
-    attrs[:orderIncrementId] = order.number
-    attrs[:orderShippingDescription] = order.shipments.first.shipping_method.name
-    attrs[:shipmentTracking] = order.shipments.first.tracking
-    attrs[:orderTotals] = order.total.to_i
-  end
-
-  def build_loop_tags
-    attrs[:'productImgUrl_#'] = order.line_items.map { |i| i.variant.images.first.attachment.url(:small) }
-    attrs[:'productName_#'] = order.line_items.map { |i| i.variant.name }
-    attrs[:'productSku_#'] = order.line_items.map { |i| i.variant.sku }
-    attrs[:'productQty_#'] = order.ine_items.map(&:quantity)
-    attrs[:'productPriceExclTax_#'] = order.line_items.map { |i| i.price.to_i }
-  end
-
 end
